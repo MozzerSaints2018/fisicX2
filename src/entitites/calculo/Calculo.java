c68e95c9b0b7188d060031026c7d6aab3c8ce82c
MozzerSaints2018 <gpossemozer2012@gmail.com>
2018-10-24 22:37:20 -0300

    FisicX com calculo IMC

--- src/entitites/calculo/Calculo.java
+++ src/entitites/calculo/Calculo.java
@@ -26,4 +26,25 @@ public class Calculo {
 	public double porcentagemGorduraMulher() {
 		return (495 - densidadeCorporalMulher()) - 495;
 	}
+	
+	public void calculoIMC() {
+		double Imc = aluno.getPeso() / (Math.sqrt(aluno.getAltura()));
+		if(Imc <= 16) {
+			System.out.println("Magreza Grave");
+		}else if(Imc > 16 && Imc <= 17) {
+			System.out.println("Magreza Moderada");
+		}else if(Imc > 17 && Imc <= 18.5) {
+			System.out.println("Magreza Leve");
+		}else if(Imc > 18.5 && Imc <= 25) {
+			System.out.println("Saudável");
+		}else if(Imc > 25 && Imc <= 30) {
+			System.out.println("Sobrepeso");
+		}else if(Imc > 30 && Imc <= 35) {
+			System.out.println("Obesidade Grau I");
+		}else if(Imc > 35 && Imc <= 40) {
+			System.out.println("Obesidade Grau II (severa)");
+		}else if(Imc > 40) {
+			System.out.println("Obesidade Grau III (mórbida)");
+		}
+	}
 }
